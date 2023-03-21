3d4f40ef648f566a34c88a324c4af54f2236c46f
Kot-Olen <125899067+Kot-Olen@users.noreply.github.com>
2023-03-21 22:41:14 +0200

    add homework

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main
+{
+    public static void main(String[] args){
+
+
+        for (int x = 1; x<=9; x++) {
+            System.out.println(" 3 * " + x + " = " + 3*x);
+        }
+
+     int result = 0;
+        for (int i = 1; i <=100; i++){
+            result = result +i;
+        }
+        System.out.println(" среднее арифметическое = " + (result/100));
+        }
+
+        int y = 456;
+    int num = 0;
+    if (y<50)
+    {System.out.println("Вы ввели слишком маленькое число");}
+        else {while (y>=50) {y/=2; num++;}
+            {System.out.println(" число : " + y);
+            }
+            {System.out.println(" количество итераций : " + y);
+            }
+        }
+        }
+
+
+
+

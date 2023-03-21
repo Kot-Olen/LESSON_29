cbce75e208e51421f3f5be350b2768c9db5d07ea
Kot-Olen <125899067+Kot-Olen@users.noreply.github.com>
2023-03-21 23:26:17 +0200

    add homework

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,39 @@
-public class Main
-{
-    public static void main(String[] args){
+public class Main {
+    public static void main(String[] args) {
 
 
-        for (int x = 1; x<=9; x++) {
-            System.out.println(" 3 * " + x + " = " + 3*x);
+        for (int x = 1; x <= 9; x++) {
+            System.out.println(" 3 * " + x + " = " + 3 * x);
         }
 
-     int result = 0;
-        for (int i = 1; i <=100; i++){
-            result = result +i;
+        int result = 0;
+        for (int i = 1; i <= 100; i++) {
+            result = result + i;
         }
-        System.out.println(" среднее арифметическое = " + (result/100));
-        }
+        System.out.println(" среднее арифметическое = " + (result / 100));
 
-        int y = 456;
-    int num = 0;
-    if (y<50)
-    {System.out.println("Вы ввели слишком маленькое число");}
-        else {while (y>=50) {y/=2; num++;}
-            {System.out.println(" число : " + y);
-            }
-            {System.out.println(" количество итераций : " + y);
-            }
-        }
-        }
 
+    int number = 456;
+    int count = 0;
+    if (number <50)
+
+    {
+        System.out.println(" Вы ввели слишком маленькое число: ");
+    }
+    else {
+        while (number >= 50) {
+            number /= 2;
+            count++;
+        }
+        {
+            System.out.println(" Число: " + number);
+        }
+        {
+            System.out.println(" Количество итераций : " + count);
+        }
+    }
+        }
+    }
 
 
 
